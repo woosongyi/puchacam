9e96b8a8c6e2d234c0218147ad29753c281f651f
우송이 <songyi107@naver.com>
2016-02-23 10:28:22 +0900

    spinner gridview 추가

--- DDu2 (2)/DDu2 (2)/app/src/main/java/com/example/administrator/ddu2/gallery/Fragment/gallery_location.java
+++ DDu2 (2)/DDu2 (2)/app/src/main/java/com/example/administrator/ddu2/gallery/Fragment/gallery_location.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.administrator.ddu2.R;
 
@@ -14,6 +18,23 @@ import com.example.administrator.ddu2.R;
 public class gallery_location extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gallery_location, container, false);
+
+        View view = inflater.inflate(R.layout.gallery_location, container, false);
+            Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),R.array.address_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),"선택된 지역 : "+parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return view;
     }
 }
